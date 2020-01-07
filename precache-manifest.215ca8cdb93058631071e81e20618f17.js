0d7440d6c865fe70bb1543ec5f94014e5ec407fc
Deya Genao <deyagenao@gmail.com>
2020-01-06 20:43:30 -0500

    Updates

---
+++ precache-manifest.215ca8cdb93058631071e81e20618f17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db4549d27c5b76b3c9b8761ee3a32d7",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "8f021863d829af59ae9b",
+    "url": "/react-memory-game/static/css/main.cb320bd3.chunk.css"
+  },
+  {
+    "revision": "7109368b4682a931cd65",
+    "url": "/react-memory-game/static/js/2.1e154929.chunk.js"
+  },
+  {
+    "revision": "8f021863d829af59ae9b",
+    "url": "/react-memory-game/static/js/main.e37ca977.chunk.js"
+  },
+  {
+    "revision": "83314d512bc0e582ba51",
+    "url": "/react-memory-game/static/js/runtime-main.fc37b826.js"
+  }
+]);
